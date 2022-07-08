1a3d7964cb8571d79d460a9bb8b318e85c028bdd
lucasr0609 <lucasram2583@gmail.com>
2022-07-08 00:01:19 -0400

    still working on project but I'm saving

---
+++ assets/js/test.js
@@ -0,0 +1,133 @@
+var startButton = document.getElementById("buttonstart")
+var questions = document.getElementById("quizquestions")
+var start = document.getElementById("starting")
+var resultSection = document.getElementById("resultsection")
+var buttonA = document.getElementById("buttonchoicea")
+var buttonB = document.getElementById("buttonchoiceb")
+var buttonC = document.getElementById("buttonchoicec")
+var buttonD = document.getElementById("buttonchoiced")
+var header = document.getElementById("topheader");
+var mainContainer = document.getElementById("quizcontainer");
+
+
+var allQuestion = [
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+
+    {
+        question: "",
+        choices: [],
+        correct: 
+    }, 
+]
